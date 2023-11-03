92b1a33e53573adb066e98e389edcf409154c652
jesidl <jesid4512@gmail.com>
2023-11-03 13:52:58 -0500

    cambios

--- frontend/src/pages/Inicio.js
+++ frontend/src/pages/Inicio.js
@@ -36,6 +36,7 @@ function Inicio() {
                     <span class="visually-hidden">Next</span>
                 </button>
             </div>
+            <a href="sms:">Abrir aplicación de SMS</a>
             <Productos />
          
         </div>
